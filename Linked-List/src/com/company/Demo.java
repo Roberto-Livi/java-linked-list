f4f8301556499e346b187fb52c4d41d9e4b07560
Roberto-Livi <robertomlivi@outlook.com>
2020-10-26 22:15:56 -0400

    worked on iterating through a list with ListIterator

--- Linked-List/src/com/company/Demo.java
+++ Linked-List/src/com/company/Demo.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
     public static void main(String[] args) {
@@ -19,6 +20,13 @@ public class Demo {
         // Inserting Record
         placesToVisit.add(1, "Clearwater");
         printList(placesToVisit);
+
+        // Removing a record
+        placesToVisit.remove(4);
+        printList(placesToVisit);
+
+
+
     }
 
     private static void printList(LinkedList<String> linkedList) {
@@ -27,5 +35,26 @@ public class Demo {
             System.out.println("Now visiting " + i.next());
         }
         System.out.println("=========================");
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+
+        while(stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
+            if(comparison == 0){
+                // equal, do not add
+                System.out.println(newCity + " is already included as a destination.");
+                return false;
+            } else if(comparison > 0){
+                // new City should appear before this one
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if(comparison < 0){
+                // move on to next city
+            }
+        }
+        stringListIterator.add(newCity);
     }
 }
